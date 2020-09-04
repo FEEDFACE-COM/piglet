6fd8a4af9e9bca0143b7a4f49e01cd8f8d57ad3d
Folkert <folkert@feedface.com>
2020-09-04 21:59:27 +0000

    format piglet.go

--- piglet.go
+++ piglet.go
@@ -2,7 +2,7 @@
 
 package piglet
 
-// #cgo CFLAGS:  -I/opt/vc/include 
+// #cgo CFLAGS:  -I/opt/vc/include
 // #cgo LDFLAGS: -L/opt/vc/lib -ldl -lbcm_host -lbrcmGLESv2 -lbrcmEGL
 // #include <stdlib.h>
 // #include "piglet.h"
@@ -10,42 +10,38 @@ import "C"
 import "unsafe"
 import "errors"
 
-
-
-func CreateContext() error { 
-    err := int(C.CreateContext())
-    if err != 0 {
-        return errors.New("fail to create context!!")
-    }
-    return nil
+func CreateContext() error {
+	err := int(C.CreateContext())
+	if err != 0 {
+		return errors.New("fail to create context!!")
+	}
+	return nil
 }
 
-func MakeCurrent() error { 
-    C.MakeCurrent()
-    return nil
+func MakeCurrent() error {
+	C.MakeCurrent()
+	return nil
 }
 
-func SwapBuffers() error { 
-    C.SwapBuffers()
-    return nil
+func SwapBuffers() error {
+	C.SwapBuffers()
+	return nil
 }
 
-
-func GetDisplaySize() (int32,int32) { 
-    return int32(C.GetDisplayWidth()), int32(C.GetDisplayHeight()) 
+func GetDisplaySize() (int32, int32) {
+	return int32(C.GetDisplayWidth()), int32(C.GetDisplayHeight())
 }
-
 
 func GetProcAddress(name string) unsafe.Pointer {
-    cname := C.CString(name)
-    defer C.free(unsafe.Pointer(cname))
-    return C.GetProcAddress(cname)    
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+	return C.GetProcAddress(cname)
 }
 
 func DestroyContext() error {
-    err := int(C.DestroyContext())
-    if err != 0 {
-        return errors.New("fail to destroy context!!")
-    }
-    return nil
+	err := int(C.DestroyContext())
+	if err != 0 {
+		return errors.New("fail to destroy context!!")
+	}
+	return nil
 }

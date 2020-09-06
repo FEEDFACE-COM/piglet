0c4d7f838c836fa696ea69123ca124753ae06c1e
Folkert <folkert@feedface.com>
2020-09-06 21:22:14 +0000

    add go doc

--- piglet.go
+++ piglet.go
@@ -1,5 +1,8 @@
 // +build linux,arm
 
+// PiGLEt provides functions to create, use and destroy an EGL rendering
+// context. OpenGLES programs drawing within in this context will be rendered
+// fullscreen to the display attached to the HDMI port of the Raspberry Pi 3.
 package piglet
 
 // #cgo CFLAGS:  -I/opt/vc/include
@@ -10,6 +13,12 @@ import "C"
 import "unsafe"
 import "errors"
 
+
+
+
+
+
+// Create a new EGL rendering context
 func CreateContext() error {
 	err := int(C.CreateContext())
 	if err != 0 {
@@ -18,26 +27,31 @@ func CreateContext() error {
 	return nil
 }
 
+// Attach the EGL rendering context to the EGL surface
 func MakeCurrent() error {
 	C.MakeCurrent()
 	return nil
 }
 
+// Post the EGL surface color buffer to the native display
 func SwapBuffers() error {
 	C.SwapBuffers()
 	return nil
 }
 
+// Return the size of the native display, in pixels
 func GetDisplaySize() (int32, int32) {
 	return int32(C.GetDisplayWidth()), int32(C.GetDisplayHeight())
 }
 
+// Return a GL or an EGL extension function
 func GetProcAddress(name string) unsafe.Pointer {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 	return C.GetProcAddress(cname)
 }
 
+// Destroy an EGL rendering context
 func DestroyContext() error {
 	err := int(C.DestroyContext())
 	if err != 0 {
